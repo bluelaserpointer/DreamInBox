f4ff8bf87ba5d8e2ba951a589b7a2a7460adc73d
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-31 17:59:59 +0800

    wip: BGMManager

---
+++ Unity/Assets/MyScripts/BGMManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class BGMManeger
+{
+    public static bool sceneChanged = true;
+
+    public static void PlayBGM(string sceneName)
+    {
+        if (sceneChanged)
+        {
+            sceneChanged = false;
+            Debug.Log("load: " + "BGM/" + sceneName);
+            AudioClip clip = Resources.Load<AudioClip>("BGM/" + sceneName);
+            if (clip != null)
+            {
+                GameObject audioSourceObject = (GameObject)Object.Instantiate(Resources.Load("PureAudioSource"));
+                AudioSource audioSource = audioSourceObject.GetComponent<AudioSource>();
+                audioSource.Stop();
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+        }
+    }
+}
