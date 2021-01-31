23f227c20ec6eea4112818b048c8c349918de0de
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-31 13:20:46 +0800

    fix: jump

---
+++ Unity/Assets/GameSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings
+{
+    public static GameSettings instance;
+
+    public static float volume = 0.5f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        volume = 0.5f;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public static void IncreaseVolume()
+    {
+        GameSettings.volume = GameSettings.volume + 0.1f > 1.0f ? 1.0f : GameSettings.volume + 0.1f;
+    }
+
+    public static void DecreaseVolume()
+    {
+        GameSettings.volume = GameSettings.volume - 0.1f < 0.0f ? 0.0f : GameSettings.volume - 0.1f;
+    }
+}
---
+++ Unity/Assets/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGrounded;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        isGrounded = true;
+    }
+
+    void OnTriggerStay()
+    {
+        isGrounded = true;
+    }
+
+    void OnTriggerExit()
+    {
+        isGrounded = false;
+    }
+}
